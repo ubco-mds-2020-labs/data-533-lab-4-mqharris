fda5806fdec8423c06f44572c5a518adad451848
RyKoe <ryan.koenig@hotmail.ca>
2020-11-26 16:07:11 -0800

    still getting used to VScode extension

--- quickscreen/analysis/datafill.py
+++ quickscreen/analysis/datafill.py
@@ -16,7 +16,25 @@ class Data_edit:
         return self.data.dtypes[column]
 
     def __add__(self, other):
-        return self.data.append(other)
+        assert isinstance(self.data, pd.core.frame.DataFrame), "Not a Pandas data frame."
+        return Data_edit(self.data.append(other, ignore_index=True))
+    
+    def __sub__(self, other):
+        assert isinstance(self.data, pd.core.frame.DataFrame), "Not a Pandas data frame."
+        temp = pd.concat([self.data, other], axis=0, join='outer')
+        common = pd.concat([self.data, temp],axis=0, join='inner', ignore_index=True).dropna(axis=0)
+        return Data_edit(common)
+
+    def rmduplicates(self):
+        return Data_edit(self.data.drop_duplicates())
+
+    def rmnan(self):
+        return Data_edit(self.data.dropna(axis=0))
+    
+    def quick_clean(self):
+        return Data_edit(self.data.drop_duplicates().dropna(axis=0))
+
+
 
 
         
--- quickscreen/analysis/linear_analysis.py
+++ quickscreen/analysis/linear_analysis.py
@@ -2,6 +2,8 @@ import column_analysis as ca
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
 
 class lm(ca.Placeholder):
     def __init__(self, data):
@@ -17,4 +19,18 @@ class lm(ca.Placeholder):
         prediction = l_regressor.predict(x)
         return prediction
 
-    
+    def singlelinearplot(self, predictor, estimator):
+        assert (isinstance(estimator, int) or estimator in self.data), "Please enter an integer or the name of a column for the estimator"
+        assert (isinstance(predictor, int) or predictor in self.data), "Please enter an integer or the name of a column for the predictor" 
+        x = self.data[estimator].to_numpy().reshape(-1,1)
+        y = self.data[predictor].to_numpy().reshape(-1,1)
+        prediction = self.singlelinear(predictor, estimator)
+        
+        plt.scatter(x, y)
+        plt.plot(x, prediction, color='red')
+        plt.title("Linear Regression")
+        plt.xlabel(estimator)
+        plt.ylabel(predictor)
+
+        return plt.show()
+
