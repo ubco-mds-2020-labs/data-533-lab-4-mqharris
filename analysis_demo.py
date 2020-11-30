1e70e57b283bfc34e17293fa37e3bcf92fee96f6
Mitch Harris <mitchharris@live.ca>
2020-11-30 14:39:16 -0800

    doc and example for sub method

--- analysis_demo.py
+++ analysis_demo.py
@@ -4,6 +4,20 @@ from quickscreen.analysis.linear_analysis import *
 from quickscreen.analysis.datafill import *
 
 if __name__ == "__main__":
+    
+
+    # sub example
 
     df = pd.read_csv("./data/CarPrice.csv")
-    print(df.head())
+    # print(df.head())
+
+    de = DataEdit(df)
+    de1 = DataEdit(df)
+
+    two_row = (df.iloc[0:2])
+    print(type(two_row.iloc[0:1]))
+    print(two_row)
+
+    d = de - two_row
+    print(df.shape)
+    print(d.data.shape)
