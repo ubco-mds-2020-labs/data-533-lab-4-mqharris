d34b45a01e1f10bf1f17d17470479f0f163f32f7
Mitch Harris <mitchharris@live.ca>
2020-11-30 17:17:18 -0800

    doc string for rm_nan

--- analysis_demo.py
+++ analysis_demo.py
@@ -1,23 +1,39 @@
 import pandas as pd
+import numpy as np
 
 from quickscreen.analysis.linear_analysis import *
 from quickscreen.analysis.datafill import *
 
 if __name__ == "__main__":
     
+    def sub_example():
+        # sub example
 
-    # sub example
+        df = pd.read_csv("./data/CarPrice.csv")
+        # print(df.head())
 
-    df = pd.read_csv("./data/CarPrice.csv")
-    # print(df.head())
+        de = DataEdit(df)
+        de1 = DataEdit(df)
 
+        two_row = (df.iloc[0:2])
+        print(type(two_row.iloc[0:1]))
+        print(two_row)
+
+        d = de - two_row
+        print(df.shape)
+        print(d.data.shape)
+
+
+    # rm_nan example
+    data = {
+            "a":[1,2,3,4,5,6,7],
+            "b":[11,12,13,14,15,np.nan,17]
+            }
+    df = pd.DataFrame(data, columns=["a", "b"])
     de = DataEdit(df)
-    de1 = DataEdit(df)
 
-    two_row = (df.iloc[0:2])
-    print(type(two_row.iloc[0:1]))
-    print(two_row)
+    print(de.data.head(10))
 
-    d = de - two_row
-    print(df.shape)
-    print(d.data.shape)
+    de_no_na = de.rm_nan()
+    print(de_no_na.data.head(10))
+
