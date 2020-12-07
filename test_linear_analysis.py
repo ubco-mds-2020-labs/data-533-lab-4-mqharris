59163fddc9b2081e0c83365fa40d37f3b6da3792
RyKoe <ryan.koenig@hotmail.ca>
2020-12-07 11:30:51 -0800

    Add test_linear_analysis

--- test_linear_analysis.py
+++ test_linear_analysis.py
@@ -1,17 +1,28 @@
 import unittest
 import pandas as pd
 import numpy as np
-import quickscreen.analysis.datafill as daf
+import quickscreen.analysis.linear_analysis as la
 
 class TestDataEdit(unittest.TestCase):
 
     def setUp(self):
-        self.data1 = daf.DataEdit(pd.DataFrame(np.arange(12).reshape(3,4)))
-        self.data2 = daf.DataEdit(pd.DataFrame(np.array([np.arange(3,7),np.arange(5,9),np.arange(3,7)])))
-        self.data3 = daf.DataEdit(pd.DataFrame(np.array([[0,1,np.nan,4],np.arange(5,9),np.arange(5,9)])))
+        self.data1 = la.Lm( )
+        self.data2 = la.Lm(pd.DataFrame(np.array([np.arange(3,7),np.arange(5,9),np.arange(3,7)])))
+        self.data3 = la.Lm(pd.DataFrame(np.array([[0,1,np.nan,4],np.arange(5,9),np.arange(5,9)])))
         
         
     def tearDown(self):
+        self.data1.destroy()
+        self.data2.destroy()
+        self.data3.destroy()
+    
+    def test_single_linear(self):
+        self.assertEqual(self.data1.single_linear(0,1), np.array([0,1,2]),'The output does not match')
+
+    def test_single_linear_eqn(self):
+        self.assertEqual(self.data1.single_linear_eqn(0,1)['Coefficient'],1,'The coefficient is wrong')
+        self.assertEqual(self.data1.single_linear_eqn(0,1)['Intercept'],0,'The coefficient is wrong')
+        
 
 
     
