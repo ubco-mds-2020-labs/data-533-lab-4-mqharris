084068425a7ed1df206e0e677d1c49071dc08c90
RyKoe <ryan.koenig@hotmail.ca>
2020-12-07 11:31:48 -0800

    fixed import path

--- quickscreen/analysis/linear_analysis.py
+++ quickscreen/analysis/linear_analysis.py
@@ -1,5 +1,4 @@
-#import quickscreen.analysis.datafill as dfl
-import datafill as dfl
+import quickscreen.analysis.datafill as dfl
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
