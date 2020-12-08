1561b9940cd01bdfad6fbae8031078c45e8a50d1
Mitch Harris <mitchharris@live.ca>
2020-12-08 15:53:18 -0800

    put tests in a file

---
+++ tests/test_grapher.py
@@ -0,0 +1,66 @@
+import unittest
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from quickscreen.plot.grapher import HistogramPlot
+from quickscreen.plot.grapher import ScatterPlot
+from quickscreen.plot.grapher import ScatterMatrix
+
+class TestGrapher(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.df = pd.DataFrame(
+            data={
+                "A":[x for x in range(0, 100)],
+                "B":[x*2 for x in range(0, 100)],
+                "C":[x*3 for x in range(0, 100)],
+                "D":[x*4 for x in range(0, 100)]
+            }
+        )
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("TearDownClass")
+    
+    def setUp(self):
+        print("SetUp")
+        self.histogram = HistogramPlot(self.df)
+        self.scatterplot = ScatterPlot(self.df)
+        self.scattermatrix = ScatterMatrix(self.df)
+    
+    def tearDown(self):
+        print("TearDown")
+        plt.clf()
+
+    def test_initialization(self):
+        self.assertIsInstance(self.histogram, HistogramPlot)
+        self.assertIsInstance(self.scattermatrix, ScatterMatrix)
+        self.assertIsInstance(self.scatterplot, ScatterPlot)
+
+    def test_grapher_data(self):
+        # histogram
+        data_hist = self.histogram.histogram("B")
+        data_per_bin = [10.0] * 10
+        bin_data = [0.,19.8,39.6,59.4,79.2,99.,118.8,138.6,158.4,178.2,198.]
+        self.assertListEqual(list(data_hist[0]), data_per_bin)
+        self.assertEqual(len(list(data_hist[1])), len(bin_data))
+        for i in range(0, len(bin_data)):
+            self.assertAlmostEqual(bin_data[i], data_hist[1][i])
+        
+        # scatter plot
+        data_sp = self.scatterplot.scatter("A", "B")[0]
+        returned_x = list(data_sp.get_xdata())
+        returned_y = list(data_sp.get_ydata())
+        expected_x = list(self.df["A"])
+        expected_y = list(self.df["B"])
+        self.assertListEqual(returned_x, expected_x)
+        self.assertListEqual(returned_y, expected_y)
+
+        # scatter matrix
+        expected_axis_0 = (-4.95, 103.95, -4.95, 103.95)
+        expected_axis_1 = (-9.9, 207.9, -4.95, 103.95)
+        data_sm = self.scattermatrix.scatter_matrix()
+        data_sm = (data_sm.axes[0])
+        self.assertEqual(expected_axis_0, data_sm[0].axis())
+        self.assertEqual(expected_axis_1, data_sm[1].axis())
---
+++ tests/test_summary_stats.py
@@ -0,0 +1,69 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+import quickscreen
+from quickscreen.summary.summary_stats import missing_summary
+from quickscreen.summary.summary_stats import stats_summary
+from quickscreen.summary.summary_stats import all_summary
+from quickscreen.summary.summary_stats import simple_summary
+
+
+class TestSummaryStats(unittest.TestCase):
+    def setUp(self):
+        print("Set Up")
+        self.df = pd.DataFrame(np.arange(100).reshape(20, 5))
+        self.df2 = self.df.copy()
+        self.df2.iloc[0, 0] = np.nan
+        self.test_array1 = np.arange(5)
+        self.test_array2 = np.arange(95, 100)
+
+    def tearDown(self):
+        print("Tear Down")
+
+    def test_missing_summary(self):
+        self.assertIsInstance(missing_summary(self.df), pd.DataFrame)
+        self.assertEqual(missing_summary(self.df).shape, (5, 2))
+        self.assertEqual(missing_summary(self.df, type="rows").shape, (20, 2))
+        self.assertEqual(missing_summary(self.df).sum().sum(), 0)
+        self.assertEqual(missing_summary(self.df2).loc[:, "count_missing"].sum(), 1)
+
+    def test_stats_summary(self):
+        self.assertIsInstance(stats_summary(self.df), pd.DataFrame)
+        self.assertEqual(stats_summary(self.df).shape, (5, 3))
+        self.assertEqual(stats_summary(self.df, type="rows").shape, (20, 3))
+        self.assertTrue(
+            (np.array(stats_summary(self.df)["min"]) == self.test_array1).all()
+        )
+        self.assertTrue(
+            (np.array(stats_summary(self.df)["max"]) == self.test_array2).all()
+        )
+        self.assertEqual(stats_summary(self.df)["mean"].mean(), 49.5)
+
+    def test_all_summary(self):
+        self.assertIsInstance(all_summary(self.df), pd.DataFrame)
+        self.assertEqual(all_summary(self.df).shape, (5, 5))
+        self.assertEqual(all_summary(self.df, type="rows").shape, (20, 5))
+        self.assertEqual(all_summary(self.df).loc[:, "count_missing"].sum(), 0)
+        self.assertEqual(all_summary(self.df2).loc[:, "count_missing"].sum(), 1)
+        self.assertTrue(
+            (np.array(all_summary(self.df)["min"]) == self.test_array1).all()
+        )
+        self.assertTrue(
+            (np.array(all_summary(self.df)["max"]) == self.test_array2).all()
+        )
+        self.assertEqual(stats_summary(self.df)["mean"].mean(), 49.5)
+
+    def test_simple_summary(self):
+        test_array3 = np.array([99.0, 0.0, 49.5, 20.0, 5.0, 0.0])
+        test_array4 = np.array([99.0, 1.0, 50.0, 20.0, 5.0, 1.0])
+        self.assertIsInstance(simple_summary(self.df), pd.DataFrame)
+        self.assertEqual(simple_summary(self.df).shape, (6, 1))
+        self.assertTrue((simple_summary(self.df).values.flatten() == test_array3).all())
+        self.assertTrue(
+            (simple_summary(self.df2).values.flatten() == test_array4).all()
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[""], verbosity=2, exit=False)
