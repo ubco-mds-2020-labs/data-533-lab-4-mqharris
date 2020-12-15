1c325a4470fefe8ac7c3917a041bc6e36d9f6c35
Mitch Harris <mitchharris@live.ca>
2020-12-15 14:51:50 -0800

    add license and setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quickscreen',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A package for exploring data',
+    url='https://github.com/mqharris/533_lab2',
+    author='Nathan Smith, Mitch Harris, Ryan Koenig',
+    author_email='blank'
+)
