add31f828531f00c80f724300d3d05aaf0acf289
Mitch Harris <mitchharris@live.ca>
2020-12-15 15:10:40 -0800

    add more files to exclude

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='quickscreen',
     version='0.1',
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(exclude=[
+        'tests*',
+         "data", 
+         "example.ipynb",
+         "description.md"
+         ]),
     license='MIT',
     description='A package for exploring data',
     url='https://github.com/mqharris/533_lab2',
