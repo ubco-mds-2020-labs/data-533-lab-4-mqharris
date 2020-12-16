9ff547138c7be7b0b5e3df69f11b3aedbe36a242
Mitch Harris <mitchharris@live.ca>
2020-12-15 16:06:16 -0800

    add email, simplify packages

--- setup.py
+++ setup.py
@@ -2,17 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='quickscreen',
-    version='0.1',
-    packages=find_packages(exclude=[
-        'tests*',
-         "data", 
-         "example.ipynb",
-         "description.md"
-         ]),
+    version='0.2',
+    packages=["quickscreen"],
     install_requires=["pandas", "numpy", "matplotlib", "seaborn", "scikit-learn"],
     license='MIT',
     description='A package for exploring data',
     url='https://github.com/mqharris/533_lab2',
     author='Nathan Smith, Mitch Harris, Ryan Koenig',
-    author_email='blank'
+    author_email='mitchharris@live.ca'
 )
